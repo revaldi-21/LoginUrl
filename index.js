38cee7dfa80ef6f5b2b7811b83a3490367813302
revaldi-21 <revaldy122@sma.belajar.id>
2025-01-26 06:18:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const rateLimiter = require('express-rate-limit');
-const compression = require('compression');
-
-app.use(compression({
-    level: 5,
-    threshold: 0,
-    filter: (req, res) => {
-        if (req.headers['x-no-compression']) {
-            return false;
-        }
-        return compression.filter(req, res);
-    }
-}));
-app.set('view engine', 'ejs');
-app.set('trust proxy', 1);
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept',
-    );
-    console.log(`[${new Date().toLocaleString()}] ${req.method} ${req.url} - ${res.statusCode}`);
-    next();
-});
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
-
 app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
     try {
@@ -38,33 +7,4 @@ app.all('/player/login/dashboard', function (req, res) {
     } catch (why) { console.log(`Warning: ${why}`); }
 
     res.render(__dirname + '/public/html/login.ejs', { data: tData });
-});
-
-
-app.all('/player/growid/login/validate', (req, res) => {
-    const _token = req.body._token;
-    const growId = req.body.growId;
-    const password = req.body.password;
-
-    const token = Buffer.from(
-        `_token=${_token}&growId=${growId}&password=${password}`,
-    ).toString('base64');
-
-    res.send(
-        `{"status":"success","message":"Account Validated.","token":"${token}","url":"","accountType":"growtopia"}`,
-    );
-});
-
-
-
-app.all('/player/*', function (req, res) {
-    res.status(301).redirect('https://api.yoruakio.tech/player/' + req.path.slice(8));
-});
-
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
-
-app.listen(5000, function () {
-    console.log('Listening on port 5000');
 });
