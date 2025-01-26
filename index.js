d4d14523a629a1f445014baf0e5bda543fc2a76c
revaldi-21 <revaldy122@sma.belajar.id>
2025-01-26 06:02:06 -0800

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,12 @@ app.all('/player/login/dashboard', function (req, res) {
 });
 
 app.all('/player/register/dashboard', function (req, res) {
-    
+    const tData = {};
+    try {
+        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|'); const uEmail = uData[2].split('|');
+        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
+        if (uName[1] && uPass[1]] && uEmail[1]) { res.redirect('/player/growid/register/validate'); }
+    } catch (why) { console.log(`Warning: ${why}`); }
     res.render(__dirname + '/public/html/register.ejs', { data: tData });
 });
 
