cd5700bf1da0f5861f159529383439f36b0fac81
revaldi-21 <revaldy122@sma.belajar.id>
2025-01-27 23:58:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -37,20 +37,8 @@ app.all('/player/login/dashboard', function (req, res) {
         if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
     } catch (why) { console.log(`Warning: ${why}`); }
 
-    res.render(__dirname + '/public/html/login.ejs', { data: tData });
+    res.render(__dirname + '/public/html/dashboard.ejs', { data: tData });
 });
-
-app.all('/player/register/dashboard', function (req, res) {
-    const tData = {};
-    try {
-        const uData = JSON.stringify(req.body).split('"')[1].split('\\n'); const uName = uData[0].split('|'); const uPass = uData[1].split('|');
-        for (let i = 0; i < uData.length - 1; i++) { const d = uData[i].split('|'); tData[d[0]] = d[1]; }
-        if (uName[1] && uPass[1]) { res.redirect('/player/growid/login/validate'); }
-    } catch (why) { console.log(`Warning: ${why}`); }
-
-    res.render(__dirname + '/public/html/register.ejs', { data: tData });
-});
-
 
 app.all('/player/growid/login/validate', (req, res) => {
 	const type = req.body.type;
